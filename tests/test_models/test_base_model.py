dcea6ea10e3ef721620ac9ddbba7c7f14f51761d
ashtkarim <assihoutkarim@gmail.com>
2024-03-18 02:48:28 +0000

    unit test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,108 +1,105 @@
 #!/usr/bin/python3
-"""Module for test BaseModel class"""
-import unittest
-from datetime import datetime
-import json
+""" Test BaseModel """
 from models.base_model import BaseModel
+import unittest
+import datetime
+from uuid import UUID
+import json
+import os
 
 
-class TestBaseModel(unittest.TestCase):
-    """ Test for BaseModel class """
+@unittest.skipIf(
+    os.getenv("HBNB_TYPE_STORAGE") == "db",
+    "Test is not relevant for BaseModel"
+)
+class test_basemodel(unittest.TestCase):
+    """ Test BaseModel """
 
-    def test_first_task(self):
-        """Test creation of class and to_dict"""
-        my_model = BaseModel()
-        self.assertIs(type(my_model), BaseModel)
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        self.assertEqual(my_model.name, "My First Model")
-        self.assertEqual(my_model.my_number, 89)
-        model_types_json = {
-            "my_number": int,
-            "name": str,
-            "__class__": str,
-            "updated_at": str,
-            "id": str,
-            "created_at": str
-        }
-        my_model_json = my_model.to_dict()
-        for key, value in model_types_json.items():
-            with self.subTest(key=key, value=value):
-                self.assertIn(key, my_model_json)
-                self.assertIs(type(my_model_json[key]), value)
+    def __init__(self, *args, **kwargs):
+        """ TEst init """
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
 
-    def test_init(self):
-        """ Test __init__ method of the BaseModel class """
-        my_model = BaseModel()
-        self.assertIsInstance(my_model, BaseModel)
-        self.assertIsInstance(my_model.id, str)
-        self.assertIsInstance(my_model.created_at, datetime)
-        self.assertIsInstance(my_model.updated_at, datetime)
+    def setUp(self):
+        """ TEst SetUp """
+        pass
 
-    def test_base_types(self):
-        """Testing dict model"""
-        second_model = BaseModel()
-        self.assertIs(type(second_model), BaseModel)
-        second_model.name = "Mira"
-        second_model.my_number = 80
-        self.assertEqual(second_model.name, "Mira")
-        self.assertEqual(second_model.my_number, 80)
-        model_types = {
-            "my_number": int,
-            "name": str,
-            "updated_at": datetime,
-            "id": str,
-            "created_at": datetime
-        }
-        for key, value in model_types.items():
-            with self.subTest(key=key, value=value):
-                self.assertIn(key, second_model.__dict__)
-                self.assertIs(type(second_model.__dict__[key]), value)
+    def tearDown(self):
+        """ Test tearDown """
+        try:
+            os.remove('file.json')
+        except Exception:
+            pass
 
-    def test_uuid(self):
-        """ testing differents uuid """
-        model = BaseModel()
-        model_2 = BaseModel()
-        self.assertNotEqual(model.id, model_2.id)
+    def test_default(self):
+        """ Test Default """
+        i = self.value()
+        self.assertEqual(type(i), self.value)
 
-    def test_datetime_model(self):
-        """testing datetime base model"""
-        model_3 = BaseModel()
-        model_4 = BaseModel()
-        self.assertNotEqual(model_3.created_at, model_3.updated_at)
-        self.assertNotEqual(model_3.created_at, model_4.created_at)
+    def test_kwargs(self):
+        """ Test Kwargs """
+        i = self.value()
+        copy = i.to_dict()
+        new = BaseModel(**copy)
+        self.assertFalse(new is i)
 
-    def test_string_representation(self):
-        """Test the magic method str"""
-        my_model = BaseModel()
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        id_model = my_model.id
+    def test_kwargs_int(self):
+        """ Test kwargs_int """
+        i = self.value()
+        copy = i.to_dict()
+        copy.update({1: 2})
+        with self.assertRaises(TypeError):
+            new = BaseModel(**copy)
 
-        expected = '[BaseModel] ({}) {}'\
-                   .format(id_model, my_model.__dict__)
-        self.assertEqual(str(my_model), expected)
+    def test_save(self):
+        """ Testing save """
+        i = self.value()
+        i.save()
+        key = self.name + "." + i.id
+        with open('file.json', 'r') as f:
+            j = json.load(f)
+            self.assertEqual(j[key], i.to_dict())
 
-    def test_constructor_kwargs(self):
-        """Test constructor that has kwargs as attributes values"""
-        obj = BaseModel()
-        obj.name = "My First Model"
-        obj.my_number = 89
-        json_attributes = obj.to_dict()
+    def test_str(self):
+        """document documt"""
+        i = self.value()
+        self.assertEqual(str(i), "[{}] ({}) {}".
+                         format(self.name, i.id, i.__dict__))
 
-        obj2 = BaseModel(**json_attributes)
+    def test_todict(self):
+        """ Test to_dict """
+        i = self.value()
+        n = i.to_dict()
+        self.assertEqual(i.to_dict(), n)
 
-        self.assertIsInstance(obj2, BaseModel)
-        self.assertIsInstance(json_attributes, dict)
-        self.assertIsNot(obj, obj2)
+    def test_kwargs_none(self):
+        """ Test kwargs """
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
 
-    def test_file_save(self):
-        """Test that info is saved to file"""
-        b3 = BaseModel()
-        b3.save()
-        with open("file.json", 'r') as f:
-            self.assertIn(b3.id, f.read())
+    def test_kwargs_one(self):
+        """document documt"""
+        n = {"name": "test"}
+        new = self.value(**n)
+        self.assertEqual(new.name, n["name"])
 
+    def test_id(self):
+        """ Test id """
+        new = self.value()
+        self.assertEqual(type(new.id), str)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_created_at(self):
+        """ TEst created_at """
+        new = self.value()
+        self.assertEqual(type(new.created_at), datetime.datetime)
+
+    def test_updated_at(self):
+        """ Test updated_at """
+        new = self.value()
+        self.assertEqual(type(new.updated_at), datetime.datetime)
+        n = new.to_dict()
+        new = BaseModel(**n)
+        new.save()
+        self.assertFalse(new.created_at == new.updated_at)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-"""Module for test User class"""
-import unittest
+""" Test """
+from tests.test_models.test_base_model import test_basemodel
 from models.user import User
-from models.base_model import BaseModel
 
 
-class TestUser(unittest.TestCase):
-    """Test User class implementation"""
+class test_User(test_basemodel):
+    """ Test """
 
-    def test_class(self):
-        """Validate the types of the attributes an class"""
-        with self.subTest(msg='Inheritance'):
-            self.assertTrue(issubclass(User, BaseModel))
+    def __init__(self, *args, **kwargs):
+        """ Test """
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
 
-        with self.subTest(msg='Attributes'):
-            self.assertIsInstance(User.email, str)
-            self.assertIsInstance(User.password, str)
-            self.assertIsInstance(User.first_name, str)
-            self.assertIsInstance(User.last_name, str)
+    def test_first_name(self):
+        """ Test """
+        new = self.value()
+        self.assertEqual(type(new.first_name), str)
 
+    def test_last_name(self):
+        """ Test """
+        new = self.value()
+        self.assertEqual(type(new.last_name), str)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_email(self):
+        """ Test """
+        new = self.value()
+        self.assertEqual(type(new.email), str)
+
+    def test_password(self):
+        """ Test """
+        new = self.value()
+        self.assertEqual(type(new.password), str)
