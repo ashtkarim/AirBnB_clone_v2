c16e7a10b67f43f85590d3f7f60f481edfbf7520
ashtkarim <assihoutkarim@gmail.com>
2024-03-20 02:27:19 +0000

    try to fix unittest

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-""" Unit test for State model """
-
-import unittest
+""" Test """
+from tests.test_models.test_base_model import test_basemodel
 from models.state import State
-from models.base_model import BaseModel
 
-# Assuming BaseModel is correctly set up for SQLAlchemy
-class TestState(unittest.TestCase):
-    """ Test State model """
 
-    def setUp(self):
-        """ Set up the test environment """
-        self.state = State()
+class test_state(test_basemodel):
+    """ Test """
 
-    def tearDown(self):
-        """ Tear down the test environment """
-        pass # Add any necessary cleanup here
+    def __init__(self, *args, **kwargs):
+        """ Test """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
 
-    def test_name_type(self):
-        """ Test that the name attribute is a string """
-        self.assertEqual(type(self.state.name), str)
-
-    def test_name_value(self):
-        """ Test that the name attribute has the correct value """
-        self.state.name = "Test State"
-        self.assertEqual(self.state.name, "Test State")
-
-    def test_updated_at(self):
-        """ Test that the updated_at attribute is correctly set """
-        # Assuming State inherits from BaseModel and has an updated_at attribute
-        self.assertIsNotNone(self.state.updated_at)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_name3(self):
+        """ Test """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
